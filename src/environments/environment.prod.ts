12b637f500d92e150e212e2f45ff08e0e68ff02b
NuranDP <mnuran@julyservices.com>
2026-06-02 00:41:33 +0600

    fix: update production API URL to correct Render service name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://worldcup-api.onrender.com/api',
+  apiUrl: 'https://worldcuppredictor-api.onrender.com/api',
   googleClientId: '436108834191-k2b7utj2gl904t7tbqog346isbhglc76.apps.googleusercontent.com',
 };
